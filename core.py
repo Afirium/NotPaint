256d8a83d9ad6c6cae9702520f8e9a757d496853
Afirium <adw.rpv@gmail.com>
2020-04-11 23:31:51 +0300

    Shapes [Line] + Optimize code

--- core.py
+++ core.py
@@ -9,8 +9,7 @@ class Shape:
         self.y1 = 0
         self.x2 = 0
         self.y2 = 0
-        self.id1 = 0
-        self.id2 = 0
+        self.id = 0
 
     # work without him
     def shape_create(self, event):
@@ -27,19 +26,15 @@ class Shape:
 
 class Rectanle(Shape):
     def shape_create(self, event):
-        x = event.x
-        y = event.y
-        c.delete(self.id1)
-        self.id2 = c.create_rectangle(self.x1, self.y1,
-                                      x, y,
-                                      fill='white')
-        self.id1 = self.id2
+        c.delete(self.id)
+        self.id = c.create_rectangle(self.x1, self.y1,
+                                     event.x, event.y,
+                                     fill='white')
         c.bind('<ButtonRelease-1>', self.shape_create_end)
 
     def shape_create_end(self, event):
         self.x2 = event.x
         self.y2 = event.y
-        c.delete(self.id2)
         c.create_rectangle(self.x1, self.y1,
                            self.x2, self.y2,
                            fill='white')
@@ -47,40 +42,61 @@ class Rectanle(Shape):
 
 class Oval(Shape):
     def shape_create(self, event):
-        x = event.x
-        y = event.y
-        c.delete(self.id1)
-        self.id2 = c.create_oval(self.x1, self.y1,
-                                 x, y,
-                                 fill='white')
-        self.id1 = self.id2
+        c.delete(self.id)
+        self.id = c.create_oval(self.x1, self.y1,
+                                event.x, event.y,
+                                fill='white')
         c.bind('<ButtonRelease-1>', self.shape_create_end)
 
     def shape_create_end(self, event):
         self.x2 = event.x
         self.y2 = event.y
-        c.delete(self.id2)
         c.create_oval(self.x1, self.y1,
                       self.x2, self.y2,
                       fill='white')
 
 
-def rectangle_select(event):
-    if flag == 0:
-        r = Rectanle()
-        c.bind('<Button-1>', r.shape_create_start)
-        c.bind('<B1-Motion>', r.shape_create)
+class Line(Shape):
+    def shape_create(self, event):
+        c.bind('<ButtonRelease-1>', self.shape_create_end)
+
+    def shape_create_start(self, event):
+        self.x1 = event.x
+        self.y1 = event.y
+
+    def shape_create_end(self, event):
+        self.x2 = event.x
+        self.y2 = event.y
+        c.create_line(self.x1, self.y1, self.x2, self.y2)
+        c.bind('<ButtonRelease-1>', c.unbind_all(event))
+
+
+def draw_rectangle(event):
+    r = Rectanle()
+    c.bind('<Button-1>', r.shape_create_start)
+    c.bind('<B1-Motion>', r.shape_create)
+
+
+def draw_oval(event):
+    o = Oval()
+    c.bind('<Button-1>', o.shape_create_start)
+    c.bind('<B1-Motion>', o.shape_create)
+
+
+def draw_line(event):
+    l = Line()
+    c.bind('<Button-1>', l.shape_create_start)
+    c.bind('<B1-Motion>', l.shape_create)
 
 
 # Parameters for TKinter
 
 main = Tk()
 main.geometry('1500x750')
-flag = 0
 main.title('NotPaint')
 main.iconbitmap('./icons/window_ico.ico')
 canvas_width = 1130
 canvas_height = 750
 canvas_color = 'white'
-c = Canvas(main, width=canvas_width, height=canvas_height, bg=canvas_color)
-c.pack(expand=NO, fill=NONE)
+c = Canvas(main, bg=canvas_color)
+c.pack(expand=1, fill=BOTH)
--- gui.py
+++ gui.py
@@ -1,11 +1,32 @@
 from tkinter import *
-from core import main, rectangle_select
+from core import main, draw_rectangle, draw_oval, draw_line
 
+# Tools buttons
 rect_icon = PhotoImage(file='./icons/ractangle.png')
-rect_btn = Button(main, bg='white', height=75,
-                  width=75, fg='black', image=rect_icon, compound=TOP)
+rect_btn = Button(main, bg='white', height=50, width=50, fg='black',
+                  image=rect_icon, compound=TOP)
 rect_btn.pack(expand=NO, fill=NONE, side=LEFT)
-rect_btn.bind('<Button-1>', rectangle_select)
-rect_btn.place(relx=0, x=2, y=100, anchor=NW)
+rect_btn.bind('<Button-1>', draw_rectangle)
+
+oval_icon = PhotoImage(file='./icons/circle.png')
+oval_btn = Button(main, bg='white', height=50,
+                  width=50, fg='black', image=oval_icon, compound=TOP)
+oval_btn.pack(expand=NO, fill=NONE, side=LEFT)
+oval_btn.bind('<Button-1>', draw_oval)
+
+line_icon = PhotoImage(file='./icons/line.png')
+line_btn = Button(main, bg='white', height=50,
+                  width=50, fg='black', image=line_icon, compound=TOP)
+line_btn.pack(expand=NO, fill=NONE, side=LEFT)
+line_btn.bind('<Button-1>', draw_line)
+
+# Menu bar
+menubar = Menu(main)
+toolsmenu = Menu(menubar, tearoff=0)
+toolsmenu.add_command(label="Show tools", command=NONE)
+toolsmenu.add_command(label="Show property menu", command=NONE)
+menubar.add_cascade(label="Tools", menu=toolsmenu)
+
+main.config(menu=menubar)
 
 mainloop()
